3761d5957518a166ee4f119802ec8b72baf97458
Mark Doughty <mdoughty@lincoln.ac.uk>
2020-03-01 17:09:40 +0000

    Added List of Property objects

--- CMP1903M_W07/Program.cs
+++ CMP1903M_W07/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CMP1903M_W07
 {
     class Program
     {
+        //List of properties held by the estate agent
+        List<Property> properties = new List<Property>();
         static void Main(string[] args)
         {
             
